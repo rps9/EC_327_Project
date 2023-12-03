9f6bbd1b9ee7b072118065c3b4e407c602965bd7
ZombieHeroX <Alan2390690731@gmail.com>
2023-12-02 20:45:54 -0500

    start screen

---
+++ app/src/main/java/com/example/ec_327_project/StartScreen.java
@@ -0,0 +1,26 @@
+package com.example.ec_327_project;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startscreen);
+
+        Button startGameButton = findViewById(R.id.startGameButton);
+        startGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start the game activity
+                Intent intent = new Intent(StartScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
